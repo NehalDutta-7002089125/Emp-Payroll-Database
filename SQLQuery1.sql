74b7bbd79953ca8d9b150ebd0f46cc6a47323530
“NehalDutta-7002089125” <nihal326dutta@gmail.com>
2023-02-18 23:01:35 +0530

    [Nehal]Add- Create values into the table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -7,3 +7,12 @@ name varchar(200) not null,
 salary money not null,
 start_Date date not null
 )
+
+Insert into EmployeeTable values
+('Rutuja',40000,'2023-02-15'),
+('Vikram',50000,'2019-05-23'),
+('Rohit',60000,'2020-02-12'),
+('Sneha',70000,'2021-03-14')
+
+
+select * from EmployeeTable
